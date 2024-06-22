3b544babea3a00afc082871c9d8a1e9f31e4628f
Tonino Lucca <tonyx1@gmail.com>
2024-06-22 17:55:00 +0200

    use oninitialized (no static)

--- BlazorApp/Components/Pages/Counter.razor.cs
+++ BlazorApp/Components/Pages/Counter.razor.cs
@@ -1,30 +1,13 @@
 using FSharpPlus.Control;
 using Microsoft.FSharp.Core;
-using SharpinoCounter;
+using static SharpinoCounter.SharpinoCounterApi;
 
 namespace ASP.Components.Pages
 {
-    // using different styles to handle the "ok" "error" of the Result. None of them are desirable at the moment. 
     public partial class Counter {
         private static Guid counterGuid = Guid.Parse("3d2d4e89-3e9b-49cc-bc15-364ab3bf9d0e");
-        private static SharpinoCounterApi.SharpinoCounterApi sharpinoCount = new SharpinoCounterApi.SharpinoCounterApi();
-        static Counter()
-        {
-            // not sure how to handle the error, so I use a plain old gard boolean value and then I will include other 
-            // possibilities to stay closer to idiomatic F# style in C# as well.
-            bool success = true;
-            success = success && sharpinoCount.AddCounter(counterGuid).IsOk;
-            success = success && sharpinoCount.Increment(counterGuid).IsOk;
-            success = success && sharpinoCount.Increment(counterGuid).IsOk;
-            success = success && sharpinoCount.Increment(counterGuid).IsOk;
-            success = success && sharpinoCount.Increment(counterGuid).IsOk;
-            if (!success) {
-                // where is the error?
-                throw new Exception("Error initializing sharpino counter");
-            }
-        }
-
-        private int currentCount = sharpinoCount.GetCounter(counterGuid).IsOk ? sharpinoCount.GetCounter(counterGuid).ResultValue.State : 0;
+        private SharpinoCounterApi sharpinoCount;
+        private int currentCount;
 
         private void IncrementCount()
         {
@@ -33,9 +16,16 @@ namespace ASP.Components.Pages
                     currentCount = sharpinoCount.GetCounter(counterGuid).ResultValue.State;
                 }
             else {
-                // what is the error?
                 throw new Exception("Error in counter");
             }
         }
+
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+            sharpinoCount = new SharpinoCounterApi();
+            currentCount = sharpinoCount.GetCounter(counterGuid).IsOk ? sharpinoCount.GetCounter(counterGuid).ResultValue.State : 0;
+        }
+
     }
 }
